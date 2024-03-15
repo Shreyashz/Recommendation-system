fde0d6efe4dba81549f9ef30fc2a28d4161e4ad0
Shreyashz <ssg1692@gmail.com>
2024-03-15 16:06:30 +0530

    combined approach of boath scaling and content-filtering

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import numpy as np
+import requests
 from scipy.spatial.distance import cosine
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
+import json
 import joblib
 
 # Load MinMaxScaler model
@@ -66,38 +68,14 @@ def Catagorical():
 
 @app.route('/recommend', methods=['POST'])
 def Recommend():
-    user_profile = request.get_json()
-    top_n = 30
-    user_profile_scaled = scaler.transform([[user_profile['followers'], user_profile['minEng'], user_profile['est_cost']]])
-    min_followers_scaled, min_engagement_scaled, max_cost_scaled = user_profile_scaled[0]
-    # print(user_profile_scaled) 
-    similarities = {}
-    for index, row in df.iterrows():
-        influencer_vector = [row['Followers'], row['EngAvg'], row['Est_Pay']]
-        user_profile_vector = [min_followers_scaled, min_engagement_scaled, max_cost_scaled]
-        similarities[row['influencer']] = 1 - cosine(influencer_vector, user_profile_vector)
-    # print(similarities)
-    sorted_influencers = sorted(similarities.items(), key=lambda x: x[1], reverse=True)[:top_n]
-    #----------------------------------------------------------------------------------------Catagorical------------------------------------------------------------
-    data = user_profile
-    user_profile_text = data['country'] + ' ' + data['category']
-    user_profile_vector = tfidf_vectorizer.transform([user_profile_text])
-    
-    similarity_scores = cosine_similarity(user_profile_vector, tfidf_matrix)
-    similarity_scores = similarity_scores.flatten()
-
-    top_indices = similarity_scores.argsort()[::-1][:top_n]
-
-    recommended_influencers = df.iloc[top_indices]
-    recommended_influencers = recommended_influencers.to_dict(orient='records')
-
-    response = {}
-    for i in sorted_influencers:
-        ind=np.where(df['influencer']==i[0])
-        # print(df.iloc[ind]['Followers'], df.iloc[ind]['EngAvg'], df.iloc[ind]['Est_Pay'])
-        print(df.iloc[ind]['Followers'], df.iloc[ind]['EngAvg'], df.iloc[ind]['Est_Pay'])
-        response[i[0]]={'followers':float(df.iloc[ind]['Followers']), 'avg_Engagement':float(df.iloc[ind]['EngAvg']), 'Estimated_pay':float(df.iloc[ind]['Est_Pay'])}
-
+    dictToSend = request.get_json()
+    response1 = requests.post('http://localhost:5000/scale', json=dictToSend)
+    response2 = requests.post('http://localhost:5000/reccat', json=dictToSend)
+    response1_json = json.loads(response1)
+    response2_json = json.loads(response2)
+    response1_keys = set(response1_json.keys())
+    response2_keys = set(response2_json.keys())
+    response = response1_keys.intersection(response2_keys)
     return jsonify({'suggested_Influencers': [response]}), 200
 
 if __name__ == '__main__':
